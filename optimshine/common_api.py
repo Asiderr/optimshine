91df7dbc2fcd5e752911576458597206afceaace
Norbert Kamiński <norbert.kaminski@infogain.com>
2025-05-13 00:34:26 +0200

    common-api: Create post request base for APIs
    Add unit tests for new class
    
    Signed-off-by: Norbert Kamiński <norbert.kaminski@infogain.com>

--- optimshine/common_api.py
+++ optimshine/common_api.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python
 
+import requests
+
+from logging import RootLogger
+
 HEADERS = {
     "Content-Type": "application/json",
     "lang": "en_US",
     "User-Agent": "Mozilla/5.0"
 }
+
+
+class CommonApi:
+    def __init__(self, log: RootLogger):
+        self.log = log
+
+    def api_post_request(self, api_url, request, token=None):
+        headers = HEADERS.copy()
+        if token:
+            headers.update({"Authorization": token})
+        response = requests.post(
+            api_url,
+            json=request,
+            headers=headers
+        )
+
+        if response.status_code != 200:
+            self.log.error(
+                f"API post failed. Status code {response.status_code}"
+            )
+            return None
+
+        return response.json()
