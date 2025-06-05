0054bb01e25332fe9b82d762b700d5a3d2d7c4cd
Norbert Kamiński <norbert.kaminski@xarium.world>
2025-06-06 01:26:17 +0200

    api_pse: Get RCE data
    Signed-off-by: Norbert Kamiński <norbert.kaminski@xarium.world>

---
+++ optimshine/api_pse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from logging import RootLogger
+from optimshine.api_common import ApiCommon
+
+
+class ApiPse(ApiCommon):
+    def __init__(self, log: RootLogger):
+        self.log = log
+
+    def get_pse_data(self, date):
+        self.log.info(f"Getting PSE RCE data for {date}.")
+        pse_url = (
+            "https://api.raporty.pse.pl/api/rce-pln?$filter="
+            f"business_date%20eq%20%27{date}%27"
+        )
+
+        response = self.api_get_request(pse_url)
+        if not response:
+            self.log.error("Getting PSE data failed!")
+            return False
+
+        try:
+            response_data = response["value"]
+        except KeyError:
+            self.log.error(f"Getting RCE values failed! {response}")
+            return False
+
+        if not response_data:
+            self.log.error("No RCE values available!")
+            return False
+
+        self.rce_data = {"date": date}
+        for quarter in response_data:
+            self.rce_data.update({quarter["udtczas"]: quarter["rce_pln"]})
+
+        self.log.info(f"Successfully obtained RCE data for {date}.")
+        return True
---
+++ tests/test_api_pse.py
@@ -0,0 +1,106 @@
+import io
+import logging
+import unittest
+
+import optimshine.api_pse as api
+
+from unittest.mock import patch
+import optimshine.optim_config as config
+
+
+class TestPseApi(unittest.TestCase):
+    def setUp(self):
+        cls_optim_config = config.OptimConfig()
+        cls_optim_config.logger_setup()
+        self.log = cls_optim_config.log
+        cls_optim_config.envs_setup("tests/.testenv")
+
+    def tearDown(self):
+        self.log.handlers.clear()
+
+    @patch("optimshine.api_common.ApiCommon.api_get_request")
+    def test_get_pse_data_none_response(self, mock_api_get_request):
+        stdio = io.StringIO()
+        mock_api_get_request.return_value = None
+
+        handler = logging.StreamHandler(stream=stdio)
+        self.log.addHandler(handler)
+        cls_api_pse = api.ApiPse(self.log)
+        status = cls_api_pse.get_pse_data("2025-04-14")
+        stdout = stdio.getvalue()
+
+        self.assertFalse(status)
+        self.assertIn("Getting PSE data failed!", stdout)
+
+    @patch("optimshine.api_common.ApiCommon.api_get_request")
+    def test_get_pse_data_wrong_response(self, mock_api_get_request):
+        stdio = io.StringIO()
+        mock_api_get_request.return_value = {"data": "Test issue"}
+
+        handler = logging.StreamHandler(stream=stdio)
+        self.log.addHandler(handler)
+        cls_api_pse = api.ApiPse(self.log)
+        status = cls_api_pse.get_pse_data("2025-04-14")
+        stdout = stdio.getvalue()
+
+        self.assertFalse(status)
+        self.assertIn("Getting RCE values failed! {'data': 'Test issue'}",
+                      stdout)
+
+    @patch("optimshine.api_common.ApiCommon.api_get_request")
+    def test_get_pse_data_empty_data(self, mock_api_get_request):
+        stdio = io.StringIO()
+        mock_api_get_request.return_value = {"value": []}
+
+        handler = logging.StreamHandler(stream=stdio)
+        self.log.addHandler(handler)
+        cls_api_pse = api.ApiPse(self.log)
+        status = cls_api_pse.get_pse_data("2025-04-14")
+        stdout = stdio.getvalue()
+
+        self.assertFalse(status)
+        self.assertIn("No RCE values available!", stdout)
+
+    @patch("optimshine.api_common.ApiCommon.api_get_request")
+    def test_get_pse_data_pass(self, mock_api_get_request):
+        mock_api_get_request.return_value = {
+            "value": [
+                {
+                  "doba": "2025-05-14",
+                  "rce_pln": 439.58,
+                  "udtczas": "2025-05-14 00:15:00",
+                  "udtczas_oreb": "00:00 - 00:15",
+                  "business_date": "2025-05-14",
+                  "source_datetime": "2025-05-13 13:49:22.204"
+                },
+                {
+                  "doba": "2025-05-14",
+                  "rce_pln": 449.58,
+                  "udtczas": "2025-05-14 00:30:00",
+                  "udtczas_oreb": "00:15 - 00:30",
+                  "business_date": "2025-05-14",
+                  "source_datetime": "2025-05-13 13:49:22.204"
+                },
+                {
+                  "doba": "2025-05-14",
+                  "rce_pln": 459.58,
+                  "udtczas": "2025-05-14 00:45:00",
+                  "udtczas_oreb": "00:30 - 00:45",
+                  "business_date": "2025-05-14",
+                  "source_datetime": "2025-05-13 13:49:22.204"
+                },
+            ]
+        }
+        expected_data = {
+            "date": "2025-05-14",
+            "2025-05-14 00:15:00": 439.58,
+            "2025-05-14 00:30:00": 449.58,
+            "2025-05-14 00:45:00": 459.58,
+        }
+
+        cls_api_pse = api.ApiPse(self.log)
+        status = cls_api_pse.get_pse_data("2025-05-14")
+
+        self.assertTrue(status)
+        self.assertTrue(hasattr(cls_api_pse, "rce_data"))
+        self.assertEqual(expected_data, cls_api_pse.rce_data)
